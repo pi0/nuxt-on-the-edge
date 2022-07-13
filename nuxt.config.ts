f51168234495948d95d21cea8164af5faecd49ff
Pooya Parsa <pooya@pi0.io>
2022-07-13 17:43:40 +0200

    update to rc.5

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,13 +2,4 @@ import { defineNuxtConfig } from 'nuxt'
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-  nitro: {
-    entry: require.resolve('./vercel-edge'),
-    minify: false,
-    rollupConfig: {
-      output: {
-        format: "module"
-      }
-    }
-  }
 })
